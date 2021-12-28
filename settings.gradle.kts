71dec2fae4937d6e62a6b59e9fe487ef24fcef88
Norman Köster <nkoester@itemis.com>
2021-12-28 13:01:27 +0100

    Add GHP to plugin repo

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,22 @@
 pluginManagement {
+
+    val gpruser: String? by extra
+    val gprtoken: String? by extra
+    val githubUsername = System.getenv("GITHUB_ACTOR") ?: gpruser as String?
+    val githubToken = System.getenv("GITHUB_TOKEN") ?: gprtoken as String?
+
+    if(githubUsername == null || githubToken == null) {
+        throw GradleException("No credentials found via envrionment variable (\$GITHUB_ACTOR/\$GITHUB_TOKEN) or via gradle properties (gpruser/gprtoken) for auth towards Github packages")
+    }
+
     repositories {
+         maven {
+            url = uri("https://maven.pkg.github.com/mbeddr/mps-gradle-plugin")
+            credentials {
+                username = githubUsername
+                password = githubToken
+            }
+        }
         maven { url = uri("https://projects.itemis.de/nexus/content/repositories/mbeddr") }
         gradlePluginPortal()
     }
