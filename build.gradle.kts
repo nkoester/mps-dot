838729f9f500c67be4361a8a458d2e3e8b8e400a
Norman Köster <nkoester@itemis.com>
2021-12-28 13:17:32 +0100

    Further refine user pwd resolution for GHP

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,11 +20,17 @@ plugins {
     id("download-jbr") version "1.5.269.964f94a"
 }
 
-// Obtain gihub credentials for github packages access
-var githubUsername = project.findProperty("gpr.user") as String? ?: System.getenv("githubUsername")
-var githubToken = project.findProperty("gpr.token") as String? ?: System.getenv("githubToken")
-
 repositories {
+    // Obtain gihub credentials for github packages access
+    val gpruser: String? by extra
+    val gprtoken: String? by extra
+    val githubUsername  = System.getenv("GITHUB_ACTOR") ?: gpruser as String?
+    val githubToken  = System.getenv("GITHUB_TOKEN") ?: gprtoken as String?
+
+    if(githubUsername == null || githubToken == null) {
+        throw GradleException("No credentials found via envrionment variable (\$GITHUB_ACTOR/\$GITHUB_TOKEN) or via gradle properties (gpruser/gprtoken) for auth towards Github packages")
+    }
+
     maven {
         url = uri("https://maven.pkg.github.com/mbeddr/mps-gradle-plugin")
         credentials {
